2e286c31b59237ffc866ec96a0f3a79f625641e3
Unknown <rtomilobil@gmail.com>
2017-10-25 17:17:47 +0200

    Create_new_person_class

---
+++ SI_assignments/SI_assignments/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI_assignments
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthDate;
+        private enum enumGender { MALE, FEMALE };
+        private enumGender gender;
+
+        public Person(string name, DateTime birthDate, int genderNr)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            switch (genderNr)
+            {
+                case 0:
+                    Gender = enumGender.MALE;
+                    break;
+                case 1:
+                    Gender = enumGender.FEMALE;
+                    break;
+            }
+        }
+
+        private enumGender Gender{
+            set { this.gender = value; }
+            get { return this.gender; }
+        }
+
+        public void toString()
+        {
+            Console.WriteLine(string.Format(
+                "Good morning.\n My name is {0} and i was born in {1} and i'm a {2}", this.name, this.birthDate, this.gender));
+        }
+    }
+}
--- SI_assignments/SI_assignments/Program.cs
+++ SI_assignments/SI_assignments/Program.cs
@@ -10,6 +10,12 @@ namespace SI_assignments
     {
         static void Main(string[] args)
         {
+            Person Béla = new Person("Béla", new DateTime(1972,02,20), 0);
+            Béla.toString();
+            Person Mari = new Person("Mari", new DateTime(1980, 03, 17), 1);
+            Mari.toString();
+            
+
         }
     }
 }
